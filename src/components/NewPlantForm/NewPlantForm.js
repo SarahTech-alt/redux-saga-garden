2ae1318e17e92540415bbcebfb94b00517dd992d
Dane Smith <doctorhowser@gmail.com>
2021-03-05 11:24:11 -0600

    Update NewPlantForm.js

--- src/components/NewPlantForm/NewPlantForm.js
+++ src/components/NewPlantForm/NewPlantForm.js
@@ -3,16 +3,20 @@ import { useDispatch } from 'react-redux';
 
 const NewPlantForm = () => {
     const dispatch = useDispatch();
+    
+    //Initial state is an OBJECT, with keys id and name
     let [newPlant, setPlant] = useState({id: 4, name: ''});
 
     const handleNameChange = (event) => {
-        console.log('event happened')
+        console.log('event happened');
+        //Similar to in redux -- we dont want to get rid of the id field when we update name
         setPlant({...newPlant, name: event.target.value})
     }
 
     const addNewPlant = event => {
         event.preventDefault();
         dispatch({ type: 'ADD_PLANT', payload: newPlant });
+        //updates the next plant to have a new id
         setPlant({id:newPlant.id + 1, name: ''});
     }
     return (
