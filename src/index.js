4a210927829310dedc6db45d73facfeef22442e7
Sarah Fuoss <sfuoss@gmail.com>
2021-09-29 17:09:09 -0500

    Imported dependencies and added a event listener for sagas.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
-
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
 import App from './App';
 
 // this startingPlantArray should eventually be removed
@@ -22,9 +23,13 @@ const plantList = (state = startingPlantArray, action) => {
   }
 };
 
+function* watcherSaga() {
+  yield takeEvery('FETCH_PLANTS', fetchPlants)
+}
+
 const store = createStore(
   combineReducers({ plantList }),
-  applyMiddleware( logger),
+  applyMiddleware( createSagaMiddleware, logger ),
 );
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('react-root'));
